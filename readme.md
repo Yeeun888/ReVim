4dec2a80f43cad5ba29a0978b78de04b799600b2
Maximus Philips Chan <108403458+Yeeun888@users.noreply.github.com>
2025-04-28 02:16:38 +1000

    First commit of the proejct. Time to get going

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 Simple vim clone with ncurses dependencies
 
 Work in progress currently before release 0.0.1
+
---
+++ src/revim.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+int main(int argc, char** argv) {
+    initscr(); 
+
+    getchar();
+
+    endwin();
+}
